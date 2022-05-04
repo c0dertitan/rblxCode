3ca6b3446cabebec2f7af059b85785784d9e0470
c0dertitan <98435530+c0dertitan@users.noreply.github.com>
2022-05-03 20:51:37 -0500

    Update README.lua

--- README.lua
+++ README.lua
@@ -1,6 +1,6 @@
 game.Workspace.Part.Touched:Connect(function()
     print('
-      Ow, I have been stepped on!')
+      Ow, I have  been stepped on!')
       
       print('
         You will pay for that!')
